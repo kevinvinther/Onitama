ded0337c01d2297481ee0d7c03119f6656eccd0d
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2022-04-24 13:05:17 +0200

    Modified: src/Lib.hs .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/Lib.dyn_hi .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/Lib.dyn_o .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/Lib.hi .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/libHSmy-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa.a .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/libHSmy-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa-ghc8.10.7.so .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/Lib.o .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/my-project-exe/my-project-exe .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/my-project-exe/my-project-exe-tmp/Main.hi .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/my-project-exe/my-project-exe-tmp/Main.o .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/my-project-test/my-project-test .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/my-project-test/my-project-test-tmp/Spec.hi .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/my-project-test/my-project-test-tmp/Spec.o .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/stack-build-caches/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/lib .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/bin/my-project-exe .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/lib/x86_64-linux-ghc-8.10.7/libHSmy-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa-ghc8.10.7.so .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/lib/x86_64-linux-ghc-8.10.7/my-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa/Lib.dyn_hi .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/lib/x86_64-linux-ghc-8.10.7/my-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa/Lib.hi .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/lib/x86_64-linux-ghc-8.10.7/my-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa/libHSmy-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa.a .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/pkgdb/my-project-0.1.0.0-1cDF4nPdXyqJINDSVvIPaa.conf .stack-work/install/x86_64-linux-tinfo6/11f99ac53a38f8317aaa73b150f52867019db7261d32e81cbaf104481da38e16/8.10.7/pkgdb/package.cache .stack-work/stack.sqlite3

--- src/Lib.hs
+++ src/Lib.hs
@@ -27,13 +27,29 @@ simulateGame file = do
     then return "InvalidFormat"
     else case stringToState (head inputLines) of
       Nothing -> return "InvalidFormat"
-      Just state -> undefined
+      Just state ->
+        if isValidState state == "None"
+          then do
+            case stringMovesToMoves [] $ stringToMove (tail inputLines) of
+              Nothing -> return "InvalidFormat"
+              Just moves -> return $ simulateGameAux state moves
+          else return $ isValidState state
 
 generateGame :: Integer -> Integer -> String
 generateGame _ _ = undefined
 
 countGames :: Integer -> FilePath -> IO String
 countGames _ _ = undefined
+
+-- | Get moves from a list of strings and return a list of moves
+stringMovesToMoves :: [GameMoves] -> [Maybe GameMoves] -> Maybe [GameMoves]
+stringMovesToMoves moves [] = Just []
+stringMovesToMoves moves (Nothing : _) = Nothing
+stringMovesToMoves moves (Just move : rest) = case stringMovesToMoves moves rest of
+  Nothing -> Nothing
+  Just restMoves -> Just (move : restMoves)
+
+-- stringMovesToMoves moves (Just move : rest) = stringMovesToMoves (moves ++ [move]) rest
 
 -- | Auxiliary method for simulateGame
 simulateGameAux :: GameState -> [GameMoves] -> String
@@ -51,6 +67,11 @@ sortCardNames :: [String] -> String
 sortCardNames [card1, card2, card3, card4, card5] =
   show (sort [card1, card2] ++ sort [card3, card4] ++ [card5])
 sortCardNames _ = ""
+
+sortCardNamesToList :: [String] -> [String]
+sortCardNamesToList [card1, card2, card3, card4, card5] =
+  sort [card1, card2] ++ sort [card3, card4] ++ [card5]
+sortCardNamesToList _ = []
 
 --------------------------------------------------------------------------------
 
@@ -125,20 +146,27 @@ pairToCoordinate (x, y) = Coordinate x y
 -----------------------------------------------------------------------------------------------------------------------
 
 isValidState :: GameState -> String
-isValidState (GameState cards@[card1, card2, card3, card4, card5] player1Coords player2Coords turn player1Super player2Super)
-  | length cards /= 5 = "InvalidFormat"
-  | length player1Coords /= 2 = "InvalidFormat"
-  | length player2Coords /= 2 = "InvalidFormat"
-  | turn < 0 || turn > 1 = "InvalidFormat"
-  | not $ all isValidPosition (player1Coords ++ player2Coords) = "InvalidFormat"
-  | length (removeDuplicates (player1Coords ++ player2Coords)) /= length (player1Coords ++ player2Coords) = "InvalidFormat"
-  | length (removeDuplicates (player1Coords ++ player2Coords)) > 10 = "InvalidFormat"
-  | otherwise = "none"
-isValidState _ = "InvalidState"
+isValidState (GameState cards player1Coords player2Coords turn player1Super player2Super)
+  | null player1Coords && null player2Coords = "InvalidState" -- no pawns
+  | length (removeDuplicates (player1Coords ++ player2Coords)) > 10 = "InvalidState" -- too many pawns
+  | length cards /= 5 = "InvalidFormat" -- not enough cards (or too many)
+  | turn < 0 || turn > 1 = "InvalidFormat" -- turn is not 0 or 1
+  | not $ all isValidPosition (player1Coords ++ player2Coords) = "InvalidFormat" -- invalid position
+  | length (removeDuplicates (player1Coords ++ player2Coords)) /= length (player1Coords ++ player2Coords) = "InvalidState" -- duplicate position
+  | sortCoordinates (tail player1Coords) /= tail player1Coords = "InvalidFormat" -- player1 pawns are not in order
+  | sortCoordinates (tail player2Coords) /= tail player2Coords = "InvalidFormat" -- player2 pawns are not in order
+  | sortCardNamesToList (map cardName cards) /= map cardName cards = "InvalidFormat" -- cards are not in order
+  | player1Coords == player2Coords = "InvalidState" -- pawns are the same
+  | head player1Coords == Coordinate 4 2 && not (null player2Coords) = "InvalidState" -- player1 has won
+  | head player2Coords == Coordinate 0 2 && not (null player1Coords) = "InvalidState" -- player2 has won
+  | otherwise = "None" -- valid state
 
 removeDuplicates :: Eq a => [a] -> [a]
 removeDuplicates [] = []
 removeDuplicates (x : xs) = x : removeDuplicates (filter (/= x) xs)
+
+sortCoordinates :: [Coordinate] -> [Coordinate]
+sortCoordinates = sortOn (\(Coordinate x y) -> (x, y))
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -146,19 +174,21 @@ removeDuplicates (x : xs) = x : removeDuplicates (filter (/= x) xs)
 
 -----------------------------------------------------------------------------------------------------------------------
 
-stringToMove :: String -> Maybe GameMoves
-stringToMove string = do
-  case stringToMoveTuple string of
-    Just move -> return (stringToMoveAux move)
-    Nothing -> Nothing
+-- FIX: For some reason stringToMoveTuple would not work, this is a (probably) temporary fix.
 
-stringToMoveTuple :: String -> Maybe ((Int, Int), (Int, Int), String)
-stringToMoveTuple string = readMaybe string :: Maybe ((Int, Int), (Int, Int), String)
+stringToMove :: [String] -> [Maybe GameMoves]
+stringToMove [] = return Nothing
+stringToMove move = do
+  string <- move
+  let readString = readMaybe string :: Maybe ((Int, Int), (Int, Int), String)
+  case readString of
+    Just move -> return (Just (stringToMoveAux move))
+    Nothing -> return Nothing
 
 stringToMoveAux :: ((Int, Int), (Int, Int), String) -> GameMoves
 stringToMoveAux ((x1, y1), (x2, y2), cardName) =
   if isPrefixOf "Super " cardName
-    then case findCard (drop 6 cardName) of
+    then case findCard (drop 6 cardName) of -- drop "Super " (with space)
       Just card -> GameMoves card (Coordinate x1 y1) (Coordinate x2 y2) True
       Nothing -> GameMoves (Card cardName []) (Coordinate x1 y1) (Coordinate x2 y2) True
     else case findCard cardName of
@@ -191,14 +221,14 @@ isValidSuperCard (Coordinate x1 y1) (Coordinate x2 y2) card allCards =
 -- | - Destination is not one of your own pawns
 -- | - You're trying to move a pawn that is yours
 isValidMove :: GameState -> GameMoves -> Bool
-isValidMove (GameState _ pawnCoordinates _ 0 _ _) (GameMoves card source destination False) =
-  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidCard 0 source destination card (take 2 allCards)
-isValidMove (GameState _ _ pawnCoordinates 1 _ _) (GameMoves card source destination False) =
-  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidCard 1 source destination card (take 2 (drop 2 allCards))
-isValidMove (GameState _ _ pawnCoordinates 0 False _) (GameMoves card source destination True) =
-  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidSuperCard source destination card (take 2 (drop 2 allCards))
-isValidMove (GameState _ _ pawnCoordinates 1 _ False) (GameMoves card source destination True) =
-  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidSuperCard source destination card (take 2 (drop 2 allCards))
+isValidMove (GameState cards pawnCoordinates opponentPawns 0 _ _) (GameMoves card source destination False) =
+  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidCard 0 source destination card (take 2 cards) && not (null opponentPawns)
+isValidMove (GameState cards opponentPawns pawnCoordinates 1 _ _) (GameMoves card source destination False) =
+  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidCard 1 source destination card (take 2 (drop 2 cards)) && not (null opponentPawns)
+isValidMove (GameState cards opponentPawns pawnCoordinates 0 False _) (GameMoves card source destination True) =
+  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidSuperCard source destination card (take 2 (drop 2 cards)) && not (null opponentPawns)
+isValidMove (GameState cards opponentPawns pawnCoordinates 1 _ False) (GameMoves card source destination True) =
+  isValidPosition source && isValidPosition destination && elem source pawnCoordinates && notElem destination pawnCoordinates && isValidSuperCard source destination card (take 2 (drop 2 cards)) && not (null opponentPawns)
 isValidMove _ _ = False
 
 hasWon :: GameState -> GameMoves -> Bool
